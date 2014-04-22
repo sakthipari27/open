2123ae303b4759eefbd3e9a07acf72cc822ad376
sakthipari27 <sakthimani.p@paripoorna.in>
2014-04-22 14:41:20 +0530

    cart
    cart

---
+++ cart/sample.master.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Data;
+using System.Configuration;
+using System.Collections;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using sample.DataTransferObject;
+using sample.BusinessLayer;
+using System.Drawing;
+
+namespace sample
+{
+    public partial class sample : System.Web.UI.MasterPage
+    {
+        /// <summary>
+        /// PageLoad Function for ProjectActivityTracker
+        /// </summary>
+        /// <param name="sender">Object Sender</param>
+        /// <param name="e">Event Arguements </param>
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+       
+    }
+}
